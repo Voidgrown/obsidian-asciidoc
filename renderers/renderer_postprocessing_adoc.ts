4cb9383b09b2a9a7d3a68db2b1f6bf9c683a2acf
Gruber Alexander <gruberx@kapsch.net>
2024-06-05 13:05:15 +0200

    Fix up CSS on reading view with a div-wrapper

--- renderers/renderer_postprocessing_adoc.ts
+++ renderers/renderer_postprocessing_adoc.ts
@@ -1,11 +1,36 @@
 import AsciiDoctor from 'asciidoctor';
 const asciidoctor = AsciiDoctor();
 
-export async function postprocessAdoc(element:string): Promise<DocumentFragment>{
+export async function postprocessAdoc(element:string): Promise<HTMLElement>{
 	// TODO: Figure out a way to do "include" statements properly here. Currently they're just, like,
 	// <a href="settings.adoc" class="bare include">settings.adoc</a>
-	// TODO: why are markdown files so much more padded than mine?
 	let html = asciidoctor.convert(element, {standalone: false} ) as string;
     const range = document.createRange();
-    return range.createContextualFragment(html.trim());
+    const fragmentsDivless = range.createContextualFragment(html.trim());
+	const ret:HTMLElement = buildNestedDivs(fragmentsDivless);
+	return ret
+}
+
+// there are several divs within the "view-content" one that, far as I can tell, add a whole lot of CSS classes. This wraps those around a fragment. 
+function buildNestedDivs(fragment:DocumentFragment): HTMLElement {
+	// this is clearly not the most elegant way of doing things, but I'm not an elegant person
+	const classesList = [
+		[ "markdown-reading-view"],
+		[ "markdown-preview-view", "markdown-rendered", "node-insert-event", "is-readable-line-width", "allow-fold-headings", "show-indentation-guide", "allow-fold-lists", "show-properties"],
+		[ "markdown-preview-sizer", "markdown-preview-section"],
+	  ];
+
+
+	let outerDiv = document.createElement('div');
+	let currentDiv = outerDiv;
+  
+	classesList.forEach(classes => {
+	  classes.forEach(className => currentDiv.classList.add(className));
+	  const innerDiv = document.createElement('div');
+	  currentDiv.appendChild(innerDiv);
+	  currentDiv = innerDiv;
+	});
+  
+	currentDiv.appendChild(fragment);
+	return outerDiv;
 }
